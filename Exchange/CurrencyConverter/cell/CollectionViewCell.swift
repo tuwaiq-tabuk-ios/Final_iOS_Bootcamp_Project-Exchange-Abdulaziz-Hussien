9204999b7bcc436bf0aa5e185886fc710a87f104
azooz alhwiti <azoozalhwiti@MacBook-Air-azooz.local>
2022-01-05 14:10:19 +0300

    P:2022/1/5 The notes interface is completed and stored in the database

---
+++ Exchange/CurrencyConverter/cell/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  CurrencyConverter
+//
+//  Created by azooz alhwiti on 27/12/2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var rate: UILabel!
+    
+   
+}
+ 
